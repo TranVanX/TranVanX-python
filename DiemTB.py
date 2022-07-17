f1fa5afee6255bc3b5f3edfeaf691a1fb3058579
ntran <tranvannhan2362001@gmail.com>
2022-07-17 15:59:53 +0700

    Tinh diem TB

---
+++ DiemTB.py
@@ -0,0 +1,8 @@
+print("Tính điểm trung bình")
+
+toan=float(input("nhập điểm toán: "))
+ly=float(input("nhập điểm lý: "))
+hoa=float(input("nhập điểm hóa: "))
+dtb=(toan+ly+hoa)/3
+print("Điểm trung bình: ",dtb)
+print("Điểm trung binh(làn tròn chữ số thập phân thứ 2:",round(dtb,2))
